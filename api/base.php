b01c54160528eca22b39bd386262f2adb3b3ff5d
mackliu <mackliu@wda.gov.tw>
2022-12-26 09:48:48 +0800

    試做find函式，並建立資料表來驗證

--- api/base.php
+++ api/base.php
@@ -14,8 +14,21 @@ class DB{
     }
 
 
-    public function find(){
+    public function find($id){
+        $sql="select * from $this->table ";
         
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql = $sql . " where " .join(" && ",$tmp);
+
+        }else{
+            $sql = $sql . " where `id`='$id'";
+        }
+
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
     public function all(){
 
@@ -53,7 +66,9 @@ function to(){
 }
 
 function q(){
-    
+
 }
 
-
+$db=new DB('bottom');
+$bot=$db->find(1);
+print_r($bot);
