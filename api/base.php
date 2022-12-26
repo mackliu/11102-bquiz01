ed0c9a5fbb99460070ff46139eff1542093c0883
mackliu <mackliu@wda.gov.tw>
2022-12-26 11:50:46 +0800

    設計幾個常用的數學函式用法

--- api/base.php
+++ api/base.php
@@ -81,19 +81,33 @@ class DB{
 
         return $this->pdo->exec($sql);
     }
-    public function sum(){
 
+    public function count(...$arg){
+        $sql="select count(*) from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->arrayToSqlArray($arg[0]);
+                $sql=$sql . " where ". join(" && ",$tmp);
+            }else{
+                $sql=$sql . $arg[0];
+            }
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetchColumn();
     }
-    public function count(){
 
+    public function sum($col,...$arg){
+        $sql="select sum($col) from $this->table ";
+
+        return $this->pdo->query($sql)->fetchColumn();
     }
     public function max(){
 
     }
-    public function min(){
+    public function min($col){
 
     }
-    public function avg(){
+    public function avg($col){
 
     }
 
@@ -131,10 +145,11 @@ $bot=$db->all();
 //print_r($db->all());
 //$db->save(['bottom'=>"2022頁尾版權"]);
 
-$row=$db->find(1);
+/* $row=$db->find(1);
 print_r($row);
 
 $row['bottom']="2023 科技大學版權所有";
 print_r($row);
 $db->save($row);
-
+ */
+echo $db->sum('price');
