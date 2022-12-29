feebd351f19ca5e413c535bb9ad1c5d76d735f29
mackliu <mackliu@wda.gov.tw>
2022-12-29 10:14:57 +0800

    新增db.sql檔案

---
+++ db04.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-12-29 03:14:34
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db04`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bottom`
+--
+
+CREATE TABLE `bottom` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bottom` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bottom`
+--
+
+INSERT INTO `bottom` (`id`, `bottom`, `price`) VALUES
+(1, '2023 科技大學版權所有', 100),
+(3, '2022頁尾版權', 200),
+(4, '2022頁尾版權', 300);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `title`
+--
+
+CREATE TABLE `title` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `title`
+--
+
+INSERT INTO `title` (`id`, `img`, `text`, `sh`) VALUES
+(1, '01B02.jpg', '泰山科技大學校園資訊系統', 1),
+(2, '01B01.jpg', '新莊科技大學', 0),
+(3, '01B04.jpg', '泰山巖', 0);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `bottom`
+--
+ALTER TABLE `bottom`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `title`
+--
+ALTER TABLE `title`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
+--
+ALTER TABLE `bottom`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `title`
+--
+ALTER TABLE `title`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
