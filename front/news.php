bf55449d7f6e22596e9c0ea22a446543f254bd99
mackliu <mackliu@wda.gov.tw>
2022-12-30 16:10:48 +0800

    顯示更多消息及分頁

--- front/news.php
+++ front/news.php
@@ -11,8 +11,52 @@
 			</marquee>
 				<div style="height:32px; display:block;"></div>
 				<!--正中央-->
+
+					<?php
+						$all=$News->count(['sh'=>1]);
+						$div=5;
+						$pages=ceil($all/$div);
+						$now=$_GET['p']??1;
+						$start=($now-1)*$div;
+						$rows=$News->all(['sh'=>1]," limit $start,$div");
+						echo "<ol start='".($start+1)."'>";
+						foreach($rows as $idx => $row){
+							echo "<li>";
+		/* 					echo $start+$idx+1 . ". "; */
+							echo mb_substr($row['text'],0,25);
+							echo "<span class='all' style='display:none;'>";
+							echo $row['text'];
+							echo "</span>";
+							echo "</li>";
+						}
+						echo "</ol>";
+
+					?>
+
+
 				<div style="text-align:center;">
-					<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-					<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
+					<?php 
+					if(($now-1)>0){
+					?>
+						<a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now-1;?>">&lt;&nbsp;</a>
+					<?php
+					}
+					?>
+
+					<?php
+					for($i=1;$i<=$pages;$i++){
+						$size=($i==$now)?"26px":"18px";
+					?>
+						<a class="bl" style="font-size:<?=$size;?>" href="?do=news&p=<?=$i;?>">&nbsp;<?=$i;?>&nbsp;</a>
+					<?php
+					}
+					?>
+					<?php 
+					if(($now+1)<=$pages){
+					?>
+					<a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now+1;?>">&nbsp;&gt;</a>
+					<?php 
+					}
+					?>
 				</div>
 			</div>
