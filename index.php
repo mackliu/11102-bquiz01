9a600d43350a2060ad53ab5e6c3d71a1ff4fc5b4
mackliu <mackliu@wda.gov.tw>
2022-12-30 13:10:18 +0800

    在前台顯示網站標題圖片及進站總人數

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ include "./api/base.php";
 	</div>
 	<iframe style="display:none;" name="back" id="back"></iframe>
 	<div id="main">
-		<a title="" href="index.php">
-			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div>
+		<a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
+			<div class="ti" style="background:url(&#39;upload/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div>
 			<!--標題-->
 		</a>
 		<div id="ms">
@@ -35,8 +35,7 @@ include "./api/base.php";
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 :
-						1 </span>
+					<span class="t">進站總人數 :<?=$Total->find(1)['total'];?> </span>
 				</div>
 			</div>
 			<?php
